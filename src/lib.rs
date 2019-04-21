453f607a481e4b301f77c0fa52df0bcfadc8a0fb
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-04-21 14:17:34 +1000

    Impl. for quoted_string

--- src/lib.rs
+++ src/lib.rs
@@ -164,6 +164,19 @@ pub fn space0<'a>() -> impl Parser<'a, Vec<char>> {
     zero_or_more(whitespace_char())
 }
 
+pub fn quoted_string<'a>() -> impl Parser<'a, String> {
+    map(
+        right(
+            match_literal("\""),
+            left(
+                zero_or_more(pred(any_char, |c| *c != '"')),
+                match_literal("\""),
+            ),
+        ),
+        |chars| chars.into_iter().collect(),
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
@@ -244,5 +257,13 @@ mod tests {
         let parser = pred(any_char, |c| *c == 'o');
         assert_eq!(Ok(("mg", 'o')), parser.parse("omg"));
         assert_eq!(Err("lol"), parser.parse("lol"));
+    }
+
+    #[test]
+    fn quoted_string_parser() {
+        assert_eq!(
+            Ok(("", "Hello Joe!".to_string())),
+            quoted_string().parse("\"Hello Joe!\"")
+        )
     }
 }
