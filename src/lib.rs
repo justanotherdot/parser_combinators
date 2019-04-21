7fe699f659e831b4133bc0757e8264ccd49c4aab
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-04-21 15:27:00 +1000

    Impl. for and_then

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,17 @@ pub trait Parser<'a, Output> {
     {
         BoxedParser::new(pred(self, pred_fn))
     }
+
+    fn and_then<F, NextP, NewOutput>(self, f: F) -> BoxedParser<'a, NewOutput>
+    where
+        Self: Sized + 'a,
+        Output: 'a,
+        NewOutput: 'a,
+        NextP: Parser<'a, NewOutput> + 'a,
+        F: Fn(Output) -> NextP + 'a,
+    {
+        BoxedParser::new(and_then(self, f))
+    }
 }
 
 impl<'a, F, Output> Parser<'a, Output> for F
@@ -235,6 +246,53 @@ pub fn single_element<'a>() -> impl Parser<'a, Element> {
         attributes,
         children: vec![],
     })
+}
+
+pub fn open_element<'a>() -> impl Parser<'a, Element> {
+    left(element_start(), match_literal(">")).map(|(name, attributes)| Element {
+        name,
+        attributes,
+        children: vec![],
+    })
+}
+
+pub fn close_element<'a>(expected_name: String) -> impl Parser<'a, String> {
+    right(match_literal("</"), left(identifier, match_literal(">")))
+        .pred(move |name| name == &expected_name)
+}
+
+//pub fn parent_element<'a>() -> impl Parser<'a, Element> {
+//pair(
+//open_element(),
+//left(zero_or_more(element()), close_element()),
+//)
+//}
+
+pub fn and_then<'a, P, F, A, B, NextP>(parser: P, f: F) -> impl Parser<'a, B>
+where
+    P: Parser<'a, A>,
+    NextP: Parser<'a, B>,
+    F: Fn(A) -> NextP,
+{
+    move |input| match parser.parse(input) {
+        Ok((next_input, result)) => f(result).parse(next_input),
+        Err(err) => Err(err),
+    }
+}
+
+pub fn either<'a, P1, P2, A>(parser1: P1, parser2: P2) -> impl Parser<'a, A>
+where
+    P1: Parser<'a, A>,
+    P2: Parser<'a, A>,
+{
+    move |input| match parser1.parse(input) {
+        ok @ Ok(_) => ok,
+        Err(_) => parser2.parse(input),
+    }
+}
+
+pub fn element<'a>() -> impl Parser<'a, Element> {
+    either(single_element(), open_element())
 }
 
 #[cfg(test)]
