b6b8636cbb14b76c8b17ce3e61c5d7dfdc2c2bed
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-04-21 11:51:25 +1000

    Impl. for identifier

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,27 @@ pub fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, (
     }
 }
 
+pub fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
@@ -33,6 +54,24 @@ mod tests {
         assert_eq!(
             Err("Hello Mike!"),
             parse_joe("Hello Mike!")
+        );
+    }
+
+    #[test]
+    fn identifier_parser() {
+        assert_eq!(
+            Ok(("", "i-am-an-identifier".to_string())),
+            identifier("i-am-an-identifier")
+        );
+
+        assert_eq!(
+            Ok((" entirely an identifier", "not".to_string())),
+            identifier("not entirely an identifier")
+        );
+
+        assert_eq!(
+            Err("!not at all an identifier"),
+            identifier("!not at all an identifier")
         );
     }
 }
