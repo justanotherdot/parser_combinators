74ea3f36154a36241462e937c229d0b24a263ab2
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-04-21 14:13:21 +1000

    Impls for whitespace parsing

--- src/lib.rs
+++ src/lib.rs
@@ -152,6 +152,18 @@ where
     }
 }
 
+pub fn whitespace_char<'a>() -> impl Parser<'a, char> {
+    pred(any_char, |c| c.is_whitespace())
+}
+
+pub fn space1<'a>() -> impl Parser<'a, Vec<char>> {
+    one_or_more(whitespace_char())
+}
+
+pub fn space0<'a>() -> impl Parser<'a, Vec<char>> {
+    zero_or_more(whitespace_char())
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
