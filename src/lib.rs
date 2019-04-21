e332d99fc679cd931fa240ea957e8313b2f4e0f9
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-04-21 11:43:54 +1000

    Impl. for match_literal

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Element {
+    name: String,
+    attributes: Vec<(String, String)>,
+    children: Vec<Element>,
+}
+
+pub fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+    move |input| match input.get(0..expected.len()) {
+        Some(next) if next == expected =>
+            Ok((&input[expected.len()..], ())),
+        _ => Err(input),
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn literal_parser() {
+        let parse_joe = match_literal("Hello Joe!");
+        assert_eq!(
+            Ok(("", ())),
+            parse_joe("Hello Joe!")
+        );
+
+        assert_eq!(
+            Ok((" Hello Robert!", ())),
+            parse_joe("Hello Joe! Hello Robert!")
+        );
+
+        assert_eq!(
+            Err("Hello Mike!"),
+            parse_joe("Hello Mike!")
+        );
     }
 }
